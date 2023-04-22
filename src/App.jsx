dfe6cb01d5109fe47faa9b68b4dbadd1daa1edd5
unknown <sebastianovalle24@gmail.com>
2023-04-22 03:13:52 -0300

    seba

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,37 @@ function App() {
 
   return (
     <>
-    <main className='md-min-h-screen'>
+    <main className='min-h-screen xsm:hidden'>
     <header>
-      <div className ='w-full h-96 bg-[url(/src/assets/images/fondo-container.png)] bg-no-repeat bg-cover bg-center'>
+      <div className ='w-full h-96 bg-[url(/src/assets/images/fondo-container.png)] bg-no-repeat bg-cover bg-center xsm:hidden'>
 
-        <div className='flex justify-between p-6'>
-         <img src="/src/assets/images/Menu.png" alt="" /> 
-         <img src="/src/assets/images/Logo.png" alt="" />
-        </div>
+        <div className='flex justify-between p-6 xsm:hidden'>
+        <div className=''>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="#564ae7 " class="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
+          </div>
+          <div className=''>
+          <img src="/src/assets/images/logo.png" alt="" className='w-10 h-10' />
+          </div>
+          </div>
 
-        <div className='w-full flex justify-center items-center flex-col'>
-          <div className='flex justify-center items-center flex-col p-2 '>
-            <img src="/src/assets/images/favorite.png" alt="" />
-            <img src="/src/assets/images/catalog.png" alt="" />
-            <img src="/src/assets/images/button read.png" alt="" />
+
+        <div className='w-full flex justify-center items-center flex-col xsm:hidden'>
+          <div className='flex justify-center items-center flex-col p-2 xsm:hidden '>
+            <img src="/src/assets/images/favorite.png" alt="" className="m-4"/>
+            <img src="/src/assets/images/catalog.png" alt="" className="m-4"/>
+            <div className="self-center"><button className="rounded-2xl text-white bg-gradient-to-b from-[#564ae7] to-[#564ae9] h-12 w-64">Stared</button></div>
+            
            </div>
         </div>
       </div>
       </header>
       
-      <div className='w-full flex justify-center mt-6'>
+      <div className='w-full flex justify-center mt-6 xsm:hidden'>
 
-        <div className='w-full flex bg-[url(/src/assets/images/frame.png)] bg-no-repeat bg-cover bg-center m-6'>
+        <div className='w-full flex bg-[url(/src/assets/images/frame.png)] bg-no-repeat bg-cover bg-center m-6 xsm:hidden'>
 
-          <div className='w-6/12 flex justify-around items-center'>
+          <div className='w-6/12 flex justify-around items-center xsm:hidden'>
            <div className='flex justify-center '>
           <img src="/src/assets/images/person.png" alt="" />
           </div>
@@ -39,24 +46,23 @@ function App() {
           <img className='mb-10' src="/src/assets/images/image-3.png" alt="" />
           </div>
           
-
           </div>
 
-          <div className='w-6/12 flex items-center'>
-          <img src="/src/assets/images/Frame-text.png" alt="" />
+          <div className='w-6/12 flex items-center xsm:hidden'>
+          <img src="/src/assets/images/Frame-text.png" alt=""className="m-3" />
           </div>
         </div>
       </div>
       </main>
       <footer>
 
-       <div className ='w-full flex justify-center mt-10'>
-          <img src="/src/assets/images/Rectangle-14.png" className='w-screen rounded-bl-full rounded-br-full' alt="" />
+       <div className ='w-full flex justify-center mt-10 xsm:hidden'>
+          <img src="/src/assets/images/Rectangle-14.png" className='w-screen h-64 rounded-bl-full rounded-br-full xsm:hidden' alt="" />
        </div>
-       <div className ='w-10/12 flex justify-center '>
+       <div className ='w-12/12 flex justify-center mb-6 xsm:hidden'>
 
-        <div className ='w-10/12 flex justify-between mt-6'>
-        <div className='w-2/12 flex justify-around'>
+        <div className ='w-10/12 flex justify-between mt-6 xsm:hidden'>
+        <div className='w-2/12 flex justify-around items-center xsm:hidden'>
         <h2>Home</h2>
         <h2>Mangas</h2>
         </div>
@@ -68,6 +74,44 @@ function App() {
        </div>
        
       </footer>
+
+     {/* movile */}
+    
+        <div className='xsm:w-full xsm:min-h-screen xsm:bg-[url(/src/assets/images/branden-movile.png)] xsm:bg-no-repeat xsm:bg-cover xsm:bg-center md:hidden'>
+
+          <div className='xsm:w-full xsm:h-96 xsm:flex xsm:justify-between xsm:pt-6 lg:hidden'>
+           <div className='xsm:m-3 '>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#564ae7" class="w-8 h-8 md:hidden">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
+          </div>
+          <div className='xsm:m-3 lg:hidden'>
+          <img src="/src/assets/images/logo.png" alt="" className='xsm:w-8 xsm:h-8' />
+          </div>
+          </div>
+
+          <div className='xsm:w-full xsm:h-16 xsm:flex xsm:justify-center xsm:flex-col'>
+
+              
+              <img src="/src/assets/images/favorite-movile.png" alt="" className='xsm:m-4' />
+
+              <img src="/src/assets/images/text-movile.png" alt="" className='xsm:m-4' />
+
+              <div className="xsm:self-center xsm:m-6 xsm:h-32 md:hidden"><button className="xsm:rounded-3xl xsm:text-white xsm:bg-gradient-to-b xsm:from-[#564ae7] xsm:to-[#564ae9] xsm:h-12 xsm:w-64">Stared</button></div>
+
+          </div>
+
+        </div>
+          
+        
+
+    
+
+     
+      
+       
+
+
+     
       
     </>
   )

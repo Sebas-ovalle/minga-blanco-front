37fba46c49a814a9958166698051ce22ac5cd17e
unknown <sebastianovalle24@gmail.com>
2023-04-25 01:50:02 -0300

    f-02

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <footer>
+    <div className ='w-full flex justify-center mt-10 xsm:hidden'>
+          <img src="/images/Rectangle-14.png"  alt="" className='w-screen h-64 rounded-bl-full rounded-br-full xsm:hidden'/>
+       </div>
+       <div className ='w-12/12 flex justify-center mb-6 xsm:hidden'>
+
+        <div className ='w-10/12 flex justify-between mt-6 xsm:hidden'>
+
+        <div className='w-2/12 flex justify-around items-center xsm:hidden'>
+        <h2>Home</h2>
+        <h2>Mangas</h2>
+        </div>
+
+        <img src="/images/Frame-logo.png" alt="" />
+        <img src="/images/Frame-sociales.png" alt="" />
+
+        
+        </div>
+    </div>
+    </footer>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     mode: 'jit',
     purge: [
-        './src//*.{js,jsx,ts,tsx}'  //esto también es una forma de englobar subdirectorios
+        './src/**/*.{js,jsx,ts,tsx}'  //esto también es una forma de englobar subdirectorios
     ],
     theme: {
         extend: {

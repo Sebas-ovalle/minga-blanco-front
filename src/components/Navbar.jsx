97e8468b0878c77114de5507e54386af6619c937
Sebastian patricio ovalle <127897018+Sebas-ovalle@users.noreply.github.com>
2023-04-25 20:51:34 -0300

    repo f-03 y f-04 (#8)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import {useState} from 'react'
 
 export default function Navbar() {
+
+  const [showMenu, setShowMenu] = useState(false)
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <header>
       <div className ='w-full h-96 bg-[url(/images/fondo-container.png)] bg-no-repeat bg-cover bg-center xsm:hidden'>
-
-        <div className='flex justify-between p-6 xsm:hidden'>
-        <div className=''>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="#564ae7 " class="w-8 h-8">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
-          </div>
-          <div className=''>
-          <img src="/images/Logo.png" alt="" className='w-10 h-10' />
-          </div>
-          </div>
+         
+      <nav className="flex w-fill justify-between">
+        <div className="relative">
+          <button onClick={handleMenuClick}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#564ae7"
+                className="w-[57px] h-[55px]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+      {showMenu && (
+        <div className="flex justify-center absolute top-full left-0 w-[500px] bg-[#564ae7]/60 rounded-md py-2 z-10">
+        <ul>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Home</a></li>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Manga</a></li>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Logout</a></li>
+        </ul>
+        </div>
+      )}    
+      </div>
+          <a href="#">
+          <img src="/images/Logo.png" className="w-[60px] h-[60px]" alt="Logo Minga" />
+          </a>
+      </nav>
 
         <div className='w-full flex justify-center items-center flex-col xsm:hidden'>
           <div className='flex justify-center items-center flex-col p-2 xsm:hidden '>
@@ -26,6 +55,7 @@ export default function Navbar() {
         </div>
         </div>
       </div>
-      </header>
+      
+    </header>
   )
 }

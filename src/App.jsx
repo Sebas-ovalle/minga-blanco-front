47bba6a4acdb5e7dc8ae5cd4c7f02fafe2183235
Sebastian patricio ovalle <127897018+Sebas-ovalle@users.noreply.github.com>
2023-04-26 01:59:24 -0300

    f-05 (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,88 @@
-import apiUrl from '../api'
 import { useState } from 'react'
 import './App.css'
 import Main from './layout/Main'
 import Index from './components/Index'
 
+import apiUrl from '../api'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
   console.log(apiUrl)
   console.log(process.env.NODE_ENV)
+  const [showMenu, setShowMenu] = useState(false);
 
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  }
   return (
     <>
-      
+    {/* HOME DESKTOP */}
      <Main> 
      <Index /> 
      </Main> 
 
-     {/* movile */}
+     {/* MOVILE */}
     
-        <div className='xsm:w-full xsm:min-h-screen xsm:bg-[url(/images/branden-movile.png)] xsm:bg-no-repeat xsm:bg-cover xsm:bg-center md:hidden'>
+        <div className='xsm:w-full xsm:h-full xsm:bg-[url(/images/branden-movile.png)] xsm:bg-no-repeat xsm:bg-cover xsm:bg-center md:hidden'>
 
-          <div className='xsm:w-full xsm:h-96 xsm:flex xsm:justify-between xsm:pt-6 lg:hidden'>
-           <div className='xsm:m-3 '>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#564ae7" class="w-8 h-8 md:hidden">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
-          </div>
-          <div className='xsm:m-3 lg:hidden'>
+          <div className='xsm:w-12/12 xsm:h-96 xsm:flex xsm:justify-between xsm:pt-6 lg:hidden flex-col'>
 
-          <img src="/images/Logo.png" alt="" className='xsm:w-8 xsm:h-8' />
-
-          </div>
-          </div>
-
-          <div className='xsm:w-full xsm:h-16 xsm:flex xsm:justify-center xsm:flex-col'>
-
-
-              
+          <nav className="flex w-fill justify-between">
+            
+          <div className="relative">
+          <button onClick={handleMenuClick}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#564ae7"
+                className="w-[57px] h-[55px] m-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+      {showMenu && (
+        <div className="flex justify-center absolute top-full left-0 w-screen h-[730px] bg-[#564ae7]/90 rounded-md py-2 z-10">
+        <ul>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Home</a></li>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Manga</a></li>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Logout</a></li>
+          <li><a className="flex px-4 py-2 text-white-800 hover:bg-violet2 font-medium" href="#">Favorite</a></li>
+        </ul>
+        </div>
+      )}    
+      </div>
+          <a href="#">
+          <img src="/images/Logo.png" className="w-[50px] h-[50px] m-4" alt="Logo Minga" />
+          </a>
+      </nav>
+          
+          <div className='xsm:w-full xsm:h-32 xsm:flex xsm:justify-center xsm:flex-col'>
               <img src="/images/favorite-movile.png" alt="" className='xsm:m-4' />
-
-
               <img src="/images/text-movile.png" alt="" className='xsm:m-4' />
-
-
               <div className="xsm:self-center xsm:m-6 xsm:h-32 md:hidden"><button className="xsm:rounded-3xl xsm:text-white xsm:bg-gradient-to-b xsm:from-[#564ae7] xsm:to-[#564ae9] xsm:h-12 xsm:w-64">Stared</button></div>
           </div>
+
+          </div>
+
+          <footer className="mt-96">
+           
+           <div className="flex items-center justify-around bg-[#564ae7]">
+           <div className="flex gap-10 text-white font-bold">
+            <h5>Home</h5>
+            <h5>Mangas</h5>
+            </div>
+            <div className="pb-5 flex items-center ">
+            <img src="/images/Frame-sociales.png" className="w-[5rem] flex items-center justify-between mt-4"/>
+            </div>
+           </div>
+            </footer>
           
         </div>
         
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -31,23 +31,37 @@ export default function Index() {
       
       <div className='w-full flex justify-center mt-6 xsm:hidden'>
 
-        <div className='w-full flex bg-[url(/images/frame.png)] bg-no-repeat bg-cover bg-center m-6 xsm:hidden' >
+        <div className='w-full flex bg-[url(/images/Frame.png)] bg-no-repeat bg-cover bg-center m-6 xsm:hidden rounded-1xl' >
 
-          <div className='w-6/12 flex justify-around items-center xsm:hidden'>
-           <div className='flex justify-center ' onClick={restar}>
-          <img className='w-[277px] h-[307px] object-cover' src={categories[counter]?.character_photo} alt="" />
-          </div>
+          <div className='w-8/12 flex justify-between items-center xsm:hidden mr-4'>
           
-          <div className='flex justify-center '>
-          <img className='w-[180px] h-[284px] mb-5' src={categories[counter]?.cover_photo} alt="" />
-          </div>
+           <svg class="h-10 w-10 text-black flex justify-center items-center" onClick={restar} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"/>
+           </svg>
+
+           <div className='flex justify-center'>
+           <img className='w-[277px] h-[307px] object-cover' src={categories[counter]?.character_photo} alt="" />
+           </div>
           
+           <div className='flex justify-center '>
+           <img className='w-[180px] h-[284px] mb-5' src={categories[counter]?.cover_photo} alt="" />
+           </div>
           </div>
 
-          <div className='w-6/12 flex items-center justify-center flex-col xsm:hidden ' onClick={sumar}>
-            <h2 className='text-2xl flex-items-center font-medium'>  {categories[counter]?.name}</h2>
-          <p className='text-sm font-sans font-normal not-italic flex item-center'>{categories[counter]?.description} </p>
+          <div className='w-6/12 flex justify-between items-center xsm:hidden ml-4'>
+           <div className=' w-8/12 flex justify-center flex-col xsm:hidden '>
+           <h2 className='text-2xl flex-items-center font-medium'>  {categories[counter]?.name}</h2>
+           <p className='text-sm font-sans font-normal not-italic flex item-center'>{categories[counter]?.description} </p>
+           </div> 
+
+           <svg class="h-10 w-10 text-black flex justify-center items-center" onClick={sumar}  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+           </svg>
           </div>
+
+          
+
+          
         </div>
       </div>
       </main>
